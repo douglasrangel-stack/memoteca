43b195e6e4ecef3420022e23e3dd3b9aa0910453
Douglas Rangel <douglascoutinho202@gmail.com>
2025-01-22 17:13:14 -0300

    add favorite

--- src/app/components/content/pensamento/pensamento.component.ts
+++ src/app/components/content/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -12,9 +13,10 @@ export class PensamentoComponent implements OnInit {
     conteudo: 'Meu primeiro pensamento',
     autoria: 'Douglas Rangel',
     modelo: 'modelo3',
+    favorito: false,
   };
 
-  constructor() {}
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {}
 
@@ -29,5 +31,16 @@ export class PensamentoComponent implements OnInit {
     }
 
     return 'pensamento-p';
+  }
+
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito) {
+      return 'ativo';
+    }
+    return 'inativo';
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
   }
 }
